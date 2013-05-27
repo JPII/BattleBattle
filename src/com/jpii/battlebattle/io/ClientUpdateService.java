186340956e5d79f7de8c1cc1553c42093cf2f91e
TexasGamer <tpgaubert@gmail.com>
2013-05-26 19:05:08 -0500

    Move ClientUpdateService out of its own thread

--- src/com/jpii/battlebattle/io/ClientUpdateService.java
+++ src/com/jpii/battlebattle/io/ClientUpdateService.java
@@ -16,7 +16,6 @@ public class ClientUpdateService {
 	
 	/* Items from client */
 	private String announcementId;
-	private BroadcastThread broadcastThread;
 	
 	/* Items from versions.xml */
 	private String versionCode, versionReadable, updateText, updateUrl;
@@ -27,9 +26,10 @@ public class ClientUpdateService {
 	
 	public ClientUpdateService() {
 		announcementId = BattleBattleIO.getAttribute("announcementId");
-		broadcastThread = new BroadcastThread();
 		
-		broadcastThread.run();
+		parseXml();
+		checkForUpdates();
+		checkForAnnouncement();
 	}
 	
 	public String getVersionCode() {
@@ -76,105 +76,88 @@ public class ClientUpdateService {
 		return hasChecked;
 	}
 	
-	class BroadcastThread extends Thread {
-		
-		public BroadcastThread() {
+	private void parseXml() {			
+		DOMParser parser = new DOMParser();
 
-		}
+        try {
+            parser.parse(new InputSource(new URL(Constants.UPDATE_URL).openStream()));
+            Document doc = parser.getDocument();
 
-		@Override
-		public void run() {
-			parseXml();
+            NodeList nodeList = doc.getElementsByTagName("battlebattle");
+            NodeList values = nodeList.item(0).getChildNodes();
+            
+            for (int i = 0; i < values.getLength(); i++) {	                
+                Node n = values.item(i);
+                NamedNodeMap m = n.getAttributes();
+                Node actualNode = n.getFirstChild();
+               
+                if (actualNode != null) {
+                	if(m.getNamedItem("name").getTextContent().equals("version_code")) {
+                		versionCode = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("version_readable")) {
+                		versionReadable = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("update_text")) {
+                		updateText = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("update_url")) {
+                		updateUrl = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("announcement_code")) {
+                		announcementCode = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("announcement_title")) {
+                		announcementTitle = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("announcement_text")) {
+                		announcementText = actualNode.getNodeValue();
+                	}
+                	
+                	if(m.getNamedItem("name").getTextContent().equals("announcement_url")) {
+                		announcementUrl = actualNode.getNodeValue();
+                	}
+                }
+            }
+
+        } catch (Exception ex) {
+            BattleBattle.getDebugger().printError("ClientUpdateService encountered an error while downloading data");
+        }
+	}
+	
+	private void checkForUpdates() {
+		try {
+			int clientVersion = Integer.parseInt(Constants.VERSION_CODE);
+			int latestVersion = Integer.parseInt(versionCode);
 			
-			checkForUpdates();
-			checkForAnnouncement();
+			if(clientVersion < latestVersion) {
+				needsUpdate = true;
+			} else {
+				BattleBattle.getDebugger().printInfo("You are running the latest version!");
+			}
+		} catch (Exception e) { }
+	}
+	
+	private void checkForAnnouncement() {
+		try {
+			int clientAnnouncement = Integer.parseInt(announcementId);
+			int latestAnnouncement = Integer.parseInt(announcementCode);
 			
-			hasChecked = true;
-		}
-		
-		private void parseXml() {			
-			DOMParser parser = new DOMParser();
-
-	        try {
-	            parser.parse(new InputSource(new URL(Constants.UPDATE_URL).openStream()));
-	            Document doc = parser.getDocument();
-
-	            NodeList nodeList = doc.getElementsByTagName("battlebattle");
-	            NodeList values = nodeList.item(0).getChildNodes();
-	            
-	            for (int i = 0; i < values.getLength(); i++) {	                
-	                Node n = values.item(i);
-	                NamedNodeMap m = n.getAttributes();
-	                Node actualNode = n.getFirstChild();
-	               
-	                if (actualNode != null) {
-	                	if(m.getNamedItem("name").getTextContent().equals("version_code")) {
-	                		versionCode = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("version_readable")) {
-	                		versionReadable = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("update_text")) {
-	                		updateText = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("update_url")) {
-	                		updateUrl = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("announcement_code")) {
-	                		announcementCode = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("announcement_title")) {
-	                		announcementTitle = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("announcement_text")) {
-	                		announcementText = actualNode.getNodeValue();
-	                	}
-	                	
-	                	if(m.getNamedItem("name").getTextContent().equals("announcement_url")) {
-	                		announcementUrl = actualNode.getNodeValue();
-	                	}
-	                }
-	            }
-
-	        } catch (Exception ex) {
-	            BattleBattle.getDebugger().printError("ClientUpdateService encountered an error while downloading data");
-	        }
-		}
-		
-		private void checkForUpdates() {
-			try {
-				int clientVersion = Integer.parseInt(Constants.VERSION_CODE);
-				int latestVersion = Integer.parseInt(versionCode);
+			if(clientAnnouncement < latestAnnouncement || latestAnnouncement == -1) {
+				hasAnnouncement = true;
 				
-				if(clientVersion < latestVersion) {
-					needsUpdate = true;
-				} else {
-					BattleBattle.getDebugger().printInfo("You are running the latest version!");
+				if(latestAnnouncement != -1) {
+					BattleBattleIO.saveAttribute("announcementId", announcementCode);
 				}
-			} catch (Exception e) { }
-		}
-		
-		private void checkForAnnouncement() {
-			try {
-				int clientAnnouncement = Integer.parseInt(announcementId);
-				int latestAnnouncement = Integer.parseInt(announcementCode);
-				
-				if(clientAnnouncement < latestAnnouncement || latestAnnouncement == -1) {
-					hasAnnouncement = true;
-					
-					if(latestAnnouncement != -1) {
-						BattleBattleIO.saveAttribute("announcementId", announcementCode);
-					}
-				} else {
-					BattleBattle.getDebugger().printInfo("No new announcements!");
-				}
-			} catch (Exception e) { }
-		}
+			} else {
+				BattleBattle.getDebugger().printInfo("No new announcements!");
+			}
+		} catch (Exception e) { }
 	}
 }
